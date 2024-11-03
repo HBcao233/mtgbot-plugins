520d7ffefa4100d4c9e6cfb7811c2aa1d94a55ca
HBcao233 <hbcaoqaq@gmail.com>
2024-11-03 16:22:25 +0800

    feat: 增加点击求解按钮时的提示

--- plugins/lighton/__init__.py
+++ plugins/lighton/__init__.py
@@ -1,12 +1,12 @@
 from telethon import events, Button, errors
 import re
 
-from util.log import logger
 from plugin import handler
 from .gauss_elimination import gen_matrix, gauss_elimination
 
 
 caption = '来用小方块填满我吧❤️～\n'
+solve_tip = '杂鱼~才这种难度就不行了吗~让小派魔教你吧，每个星星点一遍就可以过关哦'
 
 
 @handler('lighton', info='点灯游戏')
@@ -31,7 +31,8 @@ async def _event(event):
   ij = int.from_bytes(match.group(2), 'big')
   message = await event.get_message()
   buttons = message.buttons
-  if row == 255:
+
+  if row == 255:  # 求解按钮
     row = len(buttons[0])
     mat = gen_matrix(row)
     res = gauss_elimination(mat)
@@ -41,45 +42,54 @@ async def _event(event):
       buttons[i][j] = Button.inline(
         '\u2605' if (res >> i * row + j) & 1 else '\u3000', buttons[i][j].data
       )
-    await event.edit(buttons=buttons)
-  elif row == 254:
+    await event.answer(solve_tip, alert=True)
+    try:
+      await event.edit(buttons=buttons)
+    except errors.MessageNotModifiedError:
+      pass
+    return
+  elif row == 254:  # 重置按钮
     await event.edit(buttons=gen_buttons(len(buttons[0])))
-  elif row == 253:
+    return
+  elif row == 253:  # 增大按钮
     if len(buttons[0]) >= 8:
       return await event.answer('最大为8', alert=True)
     await event.edit(
       caption + f'当前大小: {len(buttons[0])+1}',
       buttons=gen_buttons(len(buttons[0]) + 1),
     )
-  elif row == 252:
+    return
+  elif row == 252:  # 减小按钮
     if len(buttons[0]) <= 1:
       return await event.answer('最小为1', alert=True)
     await event.edit(
       caption + f'当前大小: {len(buttons[0])-1}',
       buttons=gen_buttons(len(buttons[0]) - 1),
     )
+    return
+
+  # 其他按钮
+  for k in [ij, ij + 1, ij - 1, ij + row, ij - row]:
+    i = int(k / row)
+    j = k % row
+    if (
+      0 <= k < row * row
+      and (ij % row != 0 or k != ij - 1)
+      and (ij % row != row - 1 or k != ij + 1)
+    ):
+      buttons[i][j] = Button.inline(
+        '\u3000' if buttons[i][j].text == '\u2588' else '\u2588', buttons[i][j].data
+      )
+
+  if all(buttons[int(k / row)][k % row].text == '\u2588' for k in range(row * row)):
+    await event.answer('你赢啦！', alert=True)
   else:
-    for k in [ij, ij + 1, ij - 1, ij + row, ij - row]:
-      i = int(k / row)
-      j = k % row
-      if (
-        0 <= k < row * row
-        and (ij % row != 0 or k != ij - 1)
-        and (ij % row != row - 1 or k != ij + 1)
-      ):
-        buttons[i][j] = Button.inline(
-          '\u3000' if buttons[i][j].text == '\u2588' else '\u2588', buttons[i][j].data
-        )
-    if all(buttons[int(k / row)][k % row].text == '\u2588' for k in range(row * row)):
-      logger.info('won')
-      await event.answer('你赢啦！', alert=True)
+    await event.answer()
 
-    try:
-      await event.edit(buttons=buttons)
-    except errors.MessageNotModifiedError:
-      pass
-
-  await event.answer()
+  try:
+    await event.edit(buttons=buttons)
+  except errors.MessageNotModifiedError:
+    pass
 
 
 def gen_buttons(row):
