c1f332f02d8d8024e7acda9914915a59ddaa125c
HBcao233 <hbbcaoqaq@gmail.com>
2025-05-25 10:34:55 +0800

    feat: 修改发出消息内容

--- plugins/randomsese.py
+++ plugins/randomsese.py
@@ -2,22 +2,24 @@
 # @Author  : HBcao
 # @Email   : hbcaoqaq@gmail.com
 
-from telethon import types
+from telethon import types, utils
 import time
 
 import util
 from plugin import InlineCommand
 
 
-url = 'https://api.lolicon.app/setu/v2?size=regular&size=thumb&tag=萝莉&r18=1'
-
-
 @InlineCommand(' *$')
 async def _(event):
-  r = await util.get(url)
+  r = await util.get('https://api.lolicon.app/setu/v2?size=regular&size=thumb&tag=萝莉&r18=1')
   r.raise_for_status()
   res = r.json()['data'][0]
-  res = types.InputBotInlineResult(
+  
+  html_parser = utils.sanitize_parse_mode('html')
+  msg = f'<a href="https://www.pixiv.net/artworks/{res["pid"]}">pixiv_{res["pid"]}</a>'
+  message, entities = html_parser.parse(msg)
+  
+  result = types.InputBotInlineResult(
     id=str(time.time()),
     type='photo',
     thumb=types.InputWebDocument(
@@ -33,9 +35,10 @@ async def _(event):
       attributes=[],
     ),
     send_message=types.InputBotInlineMessageMediaAuto(
-      message=str(res['pid']),
+      message=message,
+      entities=entities,
     ),
     title='随机涩图',
     description='',
   )
-  return [res]
+  return [result]
