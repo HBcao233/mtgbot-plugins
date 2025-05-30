8ca5889a5a356a66471c738b2e08abb9f75d5676
HBcao233 <hbbcaoqaq@gmail.com>
2025-05-30 17:44:40 +0800

    fix: 修复25.04.28 X接口更新后无法获取推文的问题

--- plugins/twitter/data_source.py
+++ plugins/twitter/data_source.py
@@ -22,45 +22,24 @@ gheaders = {
 
 
 async def get_twitter(tid):
-  url = 'https://twitter.com/i/api/graphql/NmCeCgkVlsRGS1cAwqtgmw/TweetDetail'
+  url = 'https://twitter.com/i/api/graphql/u5Tij6ERlSH2LZvCUqallw/TweetDetail'
   variables = {
     'focalTweetId': str(tid),
     'with_rux_injections': False,
+    'rankingMode': 'Relevance',
     'includePromotedContent': True,
     'withCommunity': True,
     'withQuickPromoteEligibilityTweetFields': True,
     'withBirdwatchNotes': True,
     'withVoice': True,
-    'withV2Timeline': True,
-  }
-  features = {
-    'rweb_lists_timeline_redesign_enabled': True,
-    'responsive_web_graphql_exclude_directive_enabled': True,
-    'verified_phone_label_enabled': False,
-    'creator_subscriptions_tweet_preview_api_enabled': True,
-    'responsive_web_graphql_timeline_navigation_enabled': True,
-    'responsive_web_graphql_skip_user_profile_image_extensions_enabled': False,
-    'tweetypie_unmention_optimization_enabled': True,
-    'responsive_web_edit_tweet_api_enabled': True,
-    'graphql_is_translatable_rweb_tweet_is_translatable_enabled': True,
-    'view_counts_everywhere_api_enabled': True,
-    'longform_notetweets_consumption_enabled': True,
-    'responsive_web_twitter_article_tweet_consumption_enabled': False,
-    'tweet_awards_web_tipping_enabled': False,
-    'freedom_of_speech_not_reach_fetch_enabled': True,
-    'standardized_nudges_misinfo': True,
-    'tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled': True,
-    'longform_notetweets_rich_text_read_enabled': True,
-    'longform_notetweets_inline_media_enabled': True,
-    'responsive_web_media_download_video_enabled': False,
-    'responsive_web_enhance_cards_enabled': False,
   }
   try:
     r = await util.get(
       url,
       params={
         'variables': json.dumps(variables),
-        'features': json.dumps(features),
+        'features': '{"rweb_video_screen_enabled":false,"profile_label_improvements_pcf_label_in_post_enabled":true,"rweb_tipjar_consumption_enabled":true,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"premium_content_api_read_enabled":false,"communities_web_enable_tweet_community_results_fetch":true,"c9s_tweet_anatomy_moderator_badge_enabled":true,"responsive_web_grok_analyze_button_fetch_trends_enabled":false,"responsive_web_grok_analyze_post_followups_enabled":true,"responsive_web_jetfuel_frame":false,"responsive_web_grok_share_attachment_enabled":true,"articles_preview_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":true,"tweet_awards_web_tipping_enabled":false,"responsive_web_grok_show_grok_translated_post":false,"responsive_web_grok_analysis_button_from_backend":false,"creator_subscriptions_quote_tweet_preview_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_grok_image_annotation_enabled":true,"responsive_web_enhance_cards_enabled":false}',
+        'fieldToggles': '{"withArticleRichContentState":true,"withArticlePlainText":false,"withGrokAnalyze":false,"withDisallowedReplyControls":false}',
       },
       headers=gheaders,
     )
