4f628cb0cf3b54e68fa5ac56a00bf47a315a684a
HBcao233 <hbbcaoqaq@gmail.com>
2025-06-11 12:54:13 +0800

    fix: 修复无法解析QQ音乐分享链接的bug

--- plugins/qqmusic/__init__.py
+++ plugins/qqmusic/__init__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # @Author  : HBcao
-# @Email   : hbcaoqaq@gmail.com 
+# @Email   : hbcaoqaq@gmail.com
 # @Info    : qq音乐解析
 """requirement
 qqmusic-api-python
 """
+
 """.env.example
 # 运行 get_credential.py 扫描登录获取
 qqmusic_musicid =
@@ -19,7 +20,7 @@ from telethon import Button, types
 
 from plugin import Command
 from .data_source import (
-  get_song_info, 
+  get_song_info,
   parse_song_info,
   get_song_url,
   general_search,
@@ -31,7 +32,7 @@ import util
 
 
 _pattern = re.compile(
-  r'(?:(?:(?:https?://)?y\.qq\.com/n/ryqq/songDetail/)([0-9a-zA-Z]{12,16})|(?:c6\.y\.qq\.com/base/fcgi-bin/u\?__=/([0-9a-zA-Z]{7,7}))|^/qqmusic_song)'
+  r'(?:(?:(?:https?://)?i\.y\.qq\.com/(?:n/ryqq/songDetail/|(?:.*?songmid=)))([0-9a-zA-Z]{12,16})|(?:c6\.y\.qq\.com/base/fcgi-bin/u\?__=([0-9a-zA-Z]{7,14}))|^/qqmusic(?!_))'
 ).search
 
 
@@ -44,27 +45,27 @@ _pattern = re.compile(
 async def _song(event, mid=''):
   if not mid:
     match = event.pattern_match
-    if (mid := match.group(1)) is None and match.group(2):
+    if (mid := match.group(1)) is None and match.group(2) is None:
       return await event.reply(
         '用法: /qqmusic_song <url>',
       )
-    
+
     if match.group(2):
-      r = await util.get('https://6.y.qq.com/base/fcgi-bin/u?__=' + match.group(2))
+      r = await util.get('https://c6.y.qq.com/base/fcgi-bin/u?__=' + match.group(2))
       text = str(r.url)
-      match = _pattern(text.split('?')[0])
+      match = _pattern(text)
       mid = match.group(1)
       await event.reply(
         f'https://y.qq.com/n/ryqq/songDetail/{mid}',
       )
-    
+
   res = await get_song_info(mid)
   msg = parse_song_info(res)
-  
+
   key = f'qqmusic_{mid}'
-  singers = [i['name'] for i in res['singer']] 
+  singers = [i['name'] for i in res['singer']]
   singers = '、'.join(singers)
-  name = f"{res['title']} - {singers}"
+  name = f'{res["title"]} - {singers}'
   if not (img := util.data.Audios()[key]):
     url = await get_song_url(mid)
     img = None
@@ -79,7 +80,7 @@ async def _song(event, mid=''):
         msg,
         parse_mode='html',
       )
-      
+
     if not img:
       img = await util.getImg(url, saveas=name, ext='mp3')
   async with bot.action(event.peer_id, 'audio'):
@@ -103,24 +104,28 @@ async def _search(event):
   arr = event.raw_text.split(' ')
   if len(arr) > 1:
     keyword = arr[1]
-  
+
   if not keyword:
     async with bot.conversation(event.chat_id) as conv:
       mid = await conv.send_message(
-          '请在 60 秒内发送您想要搜索的关键词',
-          buttons=Button.text('取消', single_use=True),
+        '请在 60 秒内发送您想要搜索的关键词',
+        buttons=Button.text('取消', single_use=True),
       )
-      
+
       try:
         message = await conv.get_response()
       except asyncio.TimeoutError:
         pass
       if message.message == '取消':
         return await mid.respond('操作取消', buttons=Button.clear())
-      if not message.message or message.message.startswith('/') or len(message.message) > 64:
+      if (
+        not message.message
+        or message.message.startswith('/')
+        or len(message.message) > 64
+      ):
         return await mid.respond('输入有误', buttons=Button.clear())
       keyword = message.message.strip()
-      
+
   mid = await event.respond('请稍后...', buttons=Button.clear())
   res = await general_search(keyword)
   msg, buttons = parse_search(res)
@@ -132,10 +137,7 @@ async def _search(event):
   )
 
 
-@Command(
-  'start',
-  pattern=r'/start qqmusic_([0-9a-zA-Z]{14,14})'
-)
+@Command('start', pattern=r'/start qqmusic_([0-9a-zA-Z]{14,14})')
 async def _(event):
   match = event.pattern_match
   mid = match.group(1)
--- plugins/qqmusic/data_source.py
+++ plugins/qqmusic/data_source.py
@@ -9,7 +9,7 @@ refresh_key = config.env.get('qqmusic_refresh_key', '')
 refresh_token = config.env.get('qqmusic_refresh_token', '')
 encrypt_uin = config.env.get('qqmusic_encrypt_uin', '')
 credential = qqmusic_api.Credential(
-  musicid=musicid, 
+  musicid=musicid,
   musickey=musickey,
   refresh_key=refresh_key,
   refresh_token=refresh_token,
@@ -21,6 +21,7 @@ async def get_song_info(mid):
   res = await qqmusic_api.song.query_song(value=[mid], credential=credential)
   return res[0]
 
+
 def parse_song_info(res):
   mid = res['mid']
   url = f'https://y.qq.com/n/ryqq/songDetail/{mid}'
@@ -28,25 +29,31 @@ def parse_song_info(res):
   subtitle = res['subtitle']
   if subtitle:
     subtitle = f' ({subtitle})'
-    
+
   def parse_singer(s):
     name = s['name']
     mid = s['mid']
     url = f'https://y.qq.com/n/ryqq/singer/{mid}'
     return f'<a href="{url}">{name}</a>'
-  
+
   singers = map(parse_singer, res['singer'])
   singers = '、'.join(singers)
-  
-  msg = f'<a href="{url}">{title}</a>{subtitle} - {singers} #qqmusic\n via @%s' % bot.me.username
+
+  msg = (
+    f'<a href="{url}">{title}</a>{subtitle} - {singers} #qqmusic\n via @%s'
+    % bot.me.username
+  )
   return msg
 
 
 async def get_song_url(mid):
-  r = await util.post('https://api.toubiec.cn/api/qqmusic.php', data={
-    'url': f'https://y.qq.com/n/ryqq/songDetail/{mid}',
-    'level': 128,
-  })
+  r = await util.post(
+    'https://api.toubiec.cn/api/qqmusic.php',
+    data={
+      'url': f'https://y.qq.com/n/ryqq/songDetail/{mid}',
+      'level': 128,
+    },
+  )
   if r.status_code != 200:
     return False
   res = r.json()
@@ -58,51 +65,37 @@ async def get_song_url(mid):
 
 async def general_search(keyword):
   res = await qqmusic_api.search.general_search(
-    keyword, 
-    page=1,
-    highlight=False,
-    credential=credential
+    keyword, page=1, highlight=False, credential=credential
   )
   res = res['body']['item_song']['items']
   result = [
-    {
-      k: v
-      for k, v in d.items()
-      if k in ['mid', 'title', 'singer']
-    }
-    for d in res
+    {k: v for k, v in d.items() if k in ['mid', 'title', 'singer']} for d in res
   ]
   return result
 
 
 def parse_search(res):
   from telethon import Button
-  
+
   icons = [f'{i}\ufe0f\u20e3' for i in range(1, 10)] + ['\U0001f51f']
-  
+
   arr = [
-    f'{icons[i]} <a href="https://t.me/{bot.me.username}?start=qqmusic_{res[i]["mid"]}">{res[i]["title"]}</a> - ' +
-    '、'.join([
-      j['name']
-      for j in res[i]['singer']
-    ])
+    f'{icons[i]} <a href="https://t.me/{bot.me.username}?start=qqmusic_{res[i]["mid"]}">{res[i]["title"]}</a> - '
+    + '、'.join([j['name'] for j in res[i]['singer']])
     for i in range(10)
   ]
-  
+
   icon = '\U0001f3b5'
   urls = [f'https://t.me/{bot.me.username}?start=qqmusic_{i["mid"]}' for i in res]
-  btns = [
-    Button.url(f'{i+1} {icon}', urls[i])
-    for i in range(10)
-  ]
-  buttons = [btns[i:i+5] for i in range(0,10,5)]
+  btns = [Button.url(f'{i + 1} {icon}', urls[i]) for i in range(10)]
+  buttons = [btns[i : i + 5] for i in range(0, 10, 5)]
   return '\n'.join(arr), buttons
 
 
 async def get_try_url(res):
   url = await qqmusic_api.song.get_try_url(
-    res['mid'], 
+    res['mid'],
     vs=res['vs'][0],
     credential=credential,
   )
-  return url 
+  return url
