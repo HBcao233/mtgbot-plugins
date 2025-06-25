81602a06336a38a5c3bfe26db758974692076180
HBcao233 <hbbcaoqaq@gmail.com>
2025-06-25 09:29:22 +0800

    fix: 修bug

--- plugins/bili/__init__.py
+++ plugins/bili/__init__.py
@@ -17,8 +17,7 @@ from .data_source import get_bili, parse_msg, get_video
 
 
 _pattern = re.compile(
-  r'(?:^|^(?:/?bili(?:@%s)?) ?|(?:https?://)?bilibili\.com/video/)(av\d{1,11}|BV[0-9a-zA-Z]{8,12})|(?:b23\.tv\\?/((?![0-9]{7,7})[0-9a-zA-Z]{7,7}))|^/bili'
-  % bot.me.nickname
+  r'(?:^|^(?:/?bili) ?|(?:https?://)?bilibili\.com/video/)(av\d{1,11}|BV[0-9a-zA-Z]{8,12})|(?:b23\.tv\\?/((?![0-9]{7,7})[0-9a-zA-Z]{7,7}))|^/bili'
 ).search
 
 
