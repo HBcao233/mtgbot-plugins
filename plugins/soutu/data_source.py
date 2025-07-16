fa63e32da853cf40d21eeb3d9318f59212119c2b
HBcao233 <hbbcaoqaq@gmail.com>
2025-07-16 11:47:10 +0800

    feat: 完善 saucenao 搜索

--- plugins/soutu/data_source.py
+++ plugins/soutu/data_source.py
@@ -122,10 +122,13 @@ def parse_saucenao(res):
         f'链接: <a href="https://www.pixiv.net/artworks/{illust_id}">{title}</a>\n'
         f'画师: <a href="https://www.pixiv.net/users/{member_id}">{member_name}</a>'
       )
+
     elif index_id == 8:
+      # TODO:
+      logger.info(i)
       service_name = 'seiga'
-      member_id = results['results'][0]['data']['member_id']
-      illust_id = results['results'][0]['data']['seiga_id']
+      member_id = i['data']['member_id']
+      seiga_id = i['data']['seiga_id']
 
     elif index_id == 9 or index_id == 12:
       creator = i['data']['creator']
@@ -153,13 +156,34 @@ def parse_saucenao(res):
       msgs.append(m)
 
     elif index_id == 10:
-      service_name = 'drawr'
-      member_id = results['results'][0]['data']['member_id']
-      illust_id = results['results'][0]['data']['drawr_id']
+      # 倒闭了
+      drawr_id = i['data']['drawr_id']
+      title = i['data']['title']
+      url = f'https://drawr.net/show.php?id={drawr_id}'
+      member_id = i['data']['member_id']
+      member_name = i['data']['member_name']
+      msgs.append(
+        f'项目: Drawr\n'
+        f'<a href="{thumbnail}">预览图</a>\n'
+        f'相似度: {similarity}%\n'
+        f'链接: <a href="{url}">{title}</a>\n'
+        f'作者: {member_id} - {member_name}'
+      )
+
     elif index_id == 11:
-      service_name = 'nijie'
-      member_id = results['results'][0]['data']['member_id']
-      illust_id = results['results'][0]['data']['nijie_id']
+      nijie_id = i['data']['nijie_id']
+      title = i['data']['title']
+      url = f'https://nijie.info/view.php?id={nijie_id}'
+      member_id = i['data']['member_id']
+      member_name = i['data']['member_name']
+      member_url = f'https://sp.nijie.info/members.php?id={member_id}'
+      msgs.append(
+        f'项目: Nijie\n'
+        f'<a href="{thumbnail}">预览图</a>\n'
+        f'相似度: {similarity}%\n'
+        f'链接: <a href="{url}">{title}</a>\n'
+        f'作者: <a href="{member_url}">{member_name}</a>'
+      )
 
     elif index_id == 18:
       jp_name = i['data']['jp_name']
@@ -182,6 +206,7 @@ def parse_saucenao(res):
         f'链接: <a href="https://chan.sankakucomplex.com/post/show/{sankaku_id}">sankaku_{sankaku_id}</a>\n'
         f'作者: {creator}'
       )
+
     elif index_id == 20:
       url = i['data']['url']
       title = i['data']['title']
@@ -195,19 +220,28 @@ def parse_saucenao(res):
         f'链接: <a href="{url}">{title}</a>\n'
         f'作者: <a href="{member_url}">{member_name}</a>'
       )
+
     elif index_id == 31:
+      # 半次元 已停运
+      bcy_type = i['data']['bcy_type']
       bcy_id = i['data']['bcy_id']
-      url = f'https://bcy.net/illust/detail/{bcy_id}'
       title = i['data']['title']
-      author_name = i['data']['author_name']
-      author_url = i['data']['author_url']
+      member_link_id = i['data']['member_link_id']
+      member_url = f'https://bcy.net/coser/detail/{member_link_id}'
+      member_name = i['data']['member_name']
       msgs.append(
-        '项目: deviantArt2\n'
+        f'项目: bcy.net\n'
         f'<a href="{thumbnail}">预览图</a>\n'
         f'相似度: {similarity}%\n'
-        f'链接: <a href="{url}">{title}</a>\n'
-        f'作者: <a href="{author_url}">{author_name}</a>'
+        f'类型: {bcy_type}\n'
+        f'id及标题: {bcy_id} - {title}\n'
+        f'作者: <a href="{member_url}">{member_name}</a>'
       )
+
+    elif index_id == 32:
+      logger.info(i)
+      
+
     elif index_id == 34:
       da_id = i['data']['da_id']
       url = f'https://deviantart.com/view/{da_id}'
@@ -221,19 +255,21 @@ def parse_saucenao(res):
         f'链接: <a href="{url}">{title}</a>\n'
         f'作者: <a href="{author_url}">{author_name}</a>'
       )
-    elif index_id == 39:
-      as_project = i['data']['as_project']
-      url = f'https://www.artstation.com/artwork/{as_project}'
-      title = i['data']['title']
-      author_name = i['data']['author_name']
-      author_url = i['data']['author_url']
+
+    elif index_id == 36:
+      _type = i['data']['type']
+      mu_id = i['data']['mu_id']
+      url = f'https://www.mangaupdates.com/series.html?id={mu_id}'
+      source = i['data']['source']
+      part = i['data']['part']
       msgs.append(
-        '项目: Artstation\n'
+        f'项目: Madokami ({_type})\n'
         f'<a href="{thumbnail}">预览图</a>\n'
         f'相似度: {similarity}%\n'
-        f'链接: <a href="{url}">{title}</a>\n'
-        f'作者: <a href="{author_url}">{author_name}</a>'
+        f'part: <a href="{url}">{part}</a>\n'
+        f'source: {source}'
       )
+
     elif index_id == 37 or index_id == 371:
       md_id = i['data']['md_id']
       url = f'https://mangadex.org/chapter/{md_id}/'
@@ -254,6 +290,56 @@ def parse_saucenao(res):
       if t := i['data'].get('mal_id'):
         msg += f'\nhttps://myanimelist.net/manga/{t}/'
       msgs.append(msg)
+
+    elif index_id == 38:
+      jp_name = i['data']['jp_name']
+      eng_name = i['data']['eng_name']
+      source = i['data']['source']
+      creator = ', '.join(i['data']['creator'])
+      msgs.append(
+        '项目: H-Misc (E-Hentai)\n'
+        f'<a href="{thumbnail}">预览图</a>\n'
+        f'相似度: {similarity}%\n'
+        f'日文标题: <code>{jp_name}</code>\n'
+        f'英文标题: <code>{eng_name}</code>\n'
+        f'来源: {source}\n'
+        f'画师: {creator}'
+      )
+
+    elif index_id == 39:
+      as_project = i['data']['as_project']
+      url = f'https://www.artstation.com/artwork/{as_project}'
+      title = i['data']['title']
+      author_name = i['data']['author_name']
+      author_url = i['data']['author_url']
+      msgs.append(
+        '项目: Artstation\n'
+        f'<a href="{thumbnail}">预览图</a>\n'
+        f'相似度: {similarity}%\n'
+        f'链接: <a href="{url}">{title}</a>\n'
+        f'作者: <a href="{author_url}">{author_name}</a>'
+      )
+    elif index_id == 42:
+      # Furry Network
+      fn_type = i['data']['fn_type']
+      fn_id = i['data']['fn_id']
+      title = i['data']['title']
+      url = f'https://furrynetwork.com/artwork/{fn_id}'
+      author_url = i['data']['author_url']
+      author_name = i['data']['author_name']
+      msgs.append(
+        f'项目: Furry Network\n'
+        f'<a href="{thumbnail}">预览图</a>\n'
+        f'相似度: {similarity}%\n'
+        f'类型: {fn_type}\n'
+        f'链接: <a href="{url}">{title}</a>\n'
+        f'作者: <a href="{author_url}">{author_name}</a>'
+      )
+
+    else:
+      # TODO:
+      logger.info(i)
+
   if not res['header'].get('cache'):
     msgs.append(
       f'3s内剩余搜索次数: {short_remaining}\n24h内剩余搜索次数: {long_remaining}\n'
