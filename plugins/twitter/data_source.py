367571494fa4d228074c8f15fe1714e76244a4e9
HBcao233 <hbbcaoqaq@gmail.com>
2025-08-16 09:52:50 +0800

    feat: 更新接口 （老🐴啊！）

--- plugins/twitter/data_source.py
+++ plugins/twitter/data_source.py
@@ -1,7 +1,7 @@
 import re
 import time
 import httpx
-import ujson as json
+import json
 from datetime import datetime
 
 import config
@@ -40,12 +40,15 @@ async def get_twitter(tid):
       url,
       params={
         'variables': json.dumps(variables),
-        'features': '{"rweb_video_screen_enabled":false,"profile_label_improvements_pcf_label_in_post_enabled":true,"rweb_tipjar_consumption_enabled":true,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"premium_content_api_read_enabled":false,"communities_web_enable_tweet_community_results_fetch":true,"c9s_tweet_anatomy_moderator_badge_enabled":true,"responsive_web_grok_analyze_button_fetch_trends_enabled":false,"responsive_web_grok_analyze_post_followups_enabled":true,"responsive_web_jetfuel_frame":false,"responsive_web_grok_share_attachment_enabled":true,"articles_preview_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":true,"tweet_awards_web_tipping_enabled":false,"responsive_web_grok_show_grok_translated_post":false,"responsive_web_grok_analysis_button_from_backend":false,"creator_subscriptions_quote_tweet_preview_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_grok_image_annotation_enabled":true,"responsive_web_enhance_cards_enabled":false}',
+        'features': '{"rweb_video_screen_enabled":false,"payments_enabled":false,"rweb_xchat_enabled":false,"profile_label_improvements_pcf_label_in_post_enabled":true,"rweb_tipjar_consumption_enabled":true,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"premium_content_api_read_enabled":false,"communities_web_enable_tweet_community_results_fetch":true,"c9s_tweet_anatomy_moderator_badge_enabled":true,"responsive_web_grok_analyze_button_fetch_trends_enabled":false,"responsive_web_grok_analyze_post_followups_enabled":true,"responsive_web_jetfuel_frame":true,"responsive_web_grok_share_attachment_enabled":true,"articles_preview_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":true,"tweet_awards_web_tipping_enabled":false,"responsive_web_grok_show_grok_translated_post":false,"responsive_web_grok_analysis_button_from_backend":false,"creator_subscriptions_quote_tweet_preview_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_grok_image_annotation_enabled":true,"responsive_web_grok_imagine_annotation_enabled":true,"responsive_web_grok_community_note_auto_translation_is_enabled":false,"responsive_web_enhance_cards_enabled":false}',
         'fieldToggles': '{"withArticleRichContentState":true,"withArticlePlainText":false,"withGrokAnalyze":false,"withDisallowedReplyControls":false}',
       },
       headers=gheaders,
     )
-    res = r.json()
+    try:
+      res = r.json()
+    except json.JSONDecodeError:
+      return '解析失败，可能是老马又更新了接口'
     if 'errors' in res and len(res['errors']) > 0:
       if res['errors'][0]['code'] == 144:
         return '推文不存在'
@@ -102,7 +105,11 @@ def parse_msg(res):
   full_text = re.sub(
     r'([^@]*[^/@]+)@([0-9a-zA-Z_]*)', r'\1<a href="https://x.com/\2">@\2</a>', full_text
   )
-  if '暗号' in full_text and ('t.me' in full_text or '通道' in full_text or '直通' in full_text or '领取' in full_text or '联系' in full_text or '渠道' in full_text or '进群' in full_text or '飞机' in full_text):
+  if (
+    '暗号' in full_text or '暗语' in full_text
+  ) and (
+    't.me' in full_text or '通道' in full_text or '直通' in full_text or '领取' in full_text or '联系' in full_text or '渠道' in full_text or '进群' in full_text or '飞机' in full_text
+  ):
     full_text = f'\u26a0推文内容疑似推广诈骗，请注意甄别\n\n{full_text}'
   
   created_at = datetime.strptime(tweet['created_at'], r'%a %b %d %H:%M:%S %z %Y')
