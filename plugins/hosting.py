87915e56ec84ee51b84913af8824f94f273c64d3
HBcao233 <hbbcaoqaq@gmail.com>
2025-10-16 16:01:57 +0800

    feat: 增加picgo

--- plugins/hosting.py
+++ plugins/hosting.py
@@ -5,9 +5,12 @@ import time
 import random
 import config
 import util
+from util.log import logger
 
 
 hosting_host = config.env.get('hosting_host', '')
+picgo_api_key = config.env.get('picgo_api_key', '')
+picgo_album_id = config.env.get('picgo_album_id', '')
 hosting_root = os.path.join(config.botRoot, 'hosting')
 if hosting_host:
   u = urlparse(hosting_host)
@@ -21,6 +24,8 @@ if hosting_host:
 
 
 async def get_url(path: str, rename: str = None) -> str:
+  if picgo_api_key:
+    return await upload_picgo(path, rename)
   if hosting_host:
     return upload_local(path, rename)
   return await upload_postimage(path, rename)
@@ -46,30 +51,62 @@ async def upload_postimage(path, rename=None):
   now += '.'
   for i in range(16):
     now += str(random.randint(0, 9))
-  r = await util.post(
-    'https://postimages.org/json/rr',
-    headers={
-      'origin': 'https://postimages.org',
-      'referer': 'https://postimages.org/',
-      'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
-    },
-    data={
-      'gallery': '',
-      'optsize': 0,
-      'expire': 0,
-      'numfiles': 1,
-      'upload_session': now,
-    },
-    files={'file': file},
-  )
-  if r.status_code != 200:
-    return {'code': 1, 'message': '请求失败'}
-  res = r.json()
-  if res['status_code'] != 200:
-    return {'code': 1, 'message': '上传失败'}
+  async with util.curl.Client(
+    follow_redirects=False,
+    timeout=30,
+  ) as client:
+    r = await client.post(
+      'https://postimages.org/json/rr',
+      headers={
+        'origin': 'https://postimages.org',
+        'referer': 'https://postimages.org/',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
+      },
+      data={
+        'gallery': '',
+        'optsize': 0,
+        'expire': 0,
+        'numfiles': 1,
+        'upload_session': now,
+      },
+      files={'file': file},
+    )
+    if r.status_code != 200:
+      return {'code': 1, 'message': '请求失败'}
+    res = r.json()
+    if res['status_code'] != 200:
+      return {'code': 1, 'message': '上传失败'}
 
-  url = res['url']
-  async with util.curl.Client() as client:
+    url = res['url']
     r = await client.get(url)
   match = re.search(r'<input id="code_direct".*?value="(.*?)"', r.text)
   return match and match.group(1)
+
+
+async def upload_picgo(path, rename=None):
+  if not picgo_api_key:
+    return {'code': 1, 'message': 'picgo_api_key 未配置'}
+  file = open(path, 'rb')
+  if rename is not None:
+    file = (rename, file, '')
+
+  r = await util.post(
+    'https://www.picgo.net/api/1/upload',
+    headers={
+      'X-API-Key': picgo_api_key,
+    },
+    data={
+      'album_id': picgo_album_id,
+    },
+    files={
+      'source': file,
+    },
+  )
+  if r.status_code != 200:
+    return {'code': 1, 'message': '连接失败'}
+  res = r.json()
+  logger.debug(f'picgo: {res}')
+  if res['status_code'] != 200:
+    error = res.get('error', {}).get('message', '')
+    return {'code': 1, 'message': f'上传失败: {error}'}
+  return res['image']['url']
