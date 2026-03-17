cf5d2b2a34d8af3b82ef2d3c0cc024588bf9ffe5
HBcao233 <hbcaoqaq@outlook.com>
2026-03-17 20:43:29 +0800

    fix: save dict result

--- plugins/ehentai/data_source.py
+++ plugins/ehentai/data_source.py
@@ -360,7 +360,7 @@ class GT:
       logger.warning(f'p{i + 1} 上传失败', exc_info=1)
       return Res(f'p{i + 1} 获取失败')
     else:
-      if url:
+      if url and isinstance(url, str):
         data[key] = url
     return Res(url)
 
